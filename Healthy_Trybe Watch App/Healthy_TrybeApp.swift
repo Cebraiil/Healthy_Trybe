2b72a4132afca4b03cc7c6804b633abba6f6ed0d
cebrailblox <eymencebrail@icloud.com>
2024-02-14 13:00:52 +0300

    Initial Commit

---
+++ Healthy_Trybe Watch App/Healthy_TrybeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Healthy_TrybeApp.swift
+//  Healthy_Trybe Watch App
+//
+//  Created by Eymen on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Healthy_Trybe_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
